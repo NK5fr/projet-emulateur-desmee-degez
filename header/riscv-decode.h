fc0502d55137fe533fbae9add636ebc561bebcee
Romain Degez <romain.degez.etu@univ-lille.fr>
2024-09-18 09:11:35 +0200

    add help message and manage arguments

--- header/riscv-decode.h
+++ header/riscv-decode.h
@@ -12,3 +12,5 @@ using namespace std;
 vector<uint32_t> readFile(string filePath);
 uint32_t getOpcode(uint32_t word);
 void printResult(vector<uint32_t> words);
+void printHelp();
+void printError();
