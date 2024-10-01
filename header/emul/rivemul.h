e0028a6680b5a99751854a87675e521a14112cd8
Romain Degez <romain.degez.etu@univ-lille.fr>
2024-10-01 15:36:48 +0200

    finish to handle options

--- header/emul/rivemul.h
+++ header/emul/rivemul.h
@@ -1,4 +1,10 @@
+#include <string>
+#include <iostream>
+#include <getopt.h>
+
 using namespace std;
 
 void printHelp();
+void printDetailedHelp();
 void printError();
+void printVersion();
--- src/emul/rivemul.cpp
+++ src/emul/rivemul.cpp
@@ -1,29 +1,91 @@
 #include "../../header/emul/rivemul.h"
 
 int main(int argc, char *argv[]) {
-    if(argc != 2){
-        printHelp();
+    if(argc < 2){
+        printError();
         return 1;
     }
-    string argument(argv[1]);
-    
-    if(!argument.compare("-h")){
-        printHelp();
-        return 0;
+    int opt;
+    int resetAddr = 0x0;
+    int memSize = 512; 
+    bool interactiveMode = false;
+
+    struct option long_options[] = {
+        {"help", 0, nullptr, 1},
+        {"version", 0, nullptr, 'V'}
+    };
+    while ((opt = getopt_long(argc, argv, "r:im:hV", long_options, nullptr)) != -1) {
+        switch (opt) {
+            case 'r':
+                resetAddr = stoi(optarg, 0);
+                break;
+            case 'i':
+                interactiveMode = true;
+                break;
+            case 'm':
+                memSize = stoi(optarg);
+                break;
+            case 'h':
+                printHelp();
+                return 0;
+            case 1:
+                printDetailedHelp();
+                return 0;
+            case 'V':
+                printVersion();
+                return 0;
+            default:
+                printError();
+                return 1;
+        }
     }
+
+    cout << argv[optind] << endl;
+
     return 0; 
 }
 
 void printHelp(){
-    cout << "Un désassembleur RISC-V pour le jeu d'instruction RV32I" << endl;
-    cout << "\nUtilisation: riscv-disas [OPTIONS] FICHIER_BIN" << endl;
+    cout << "Rivemul is a terminal simple emulator of the RISC-V RV32I architecture" << endl;
+    cout << "\nUsage: rivemul [OPTIONS] [MEMORY_SEGMENT]..." << endl;
     cout << "\nArguments:" << endl;
-    cout << "   FICHIER_BIN         Un fichier contenant les instructions à désassembler" << endl;
+    cout << "   [MEMORY_SEGMENT]...   The files to load into memory. Format is file:base_addr.  If :base_addr is not provided, defaults to 0. The files and their base_addr must fit somewhere in the two memory banks" << endl;
     cout << "\nOptions:" << endl;
-    cout << "   -h                  Affiche ce message d'aide" << endl;
+    cout << "   -r <RESET_ADDR>       The reset address [default: 0x0]" << endl;
+    cout << "   -i                    Interactive (debug) mode" << endl;
+    cout << "   -m <MEM_SIZE>         Memory size (in KB). This memory size will be split in 2 memory banks: [default: 512]" << endl;
+    cout << "   -h, --help            Print help (see more with '--help')" << endl;
+    cout << "   -V, --version         Print version" << endl;
+}
+
+void printDetailedHelp(){
+    cout << "Rivemul is a terminal simple emulator of the RISC-V RV32I architecture" << endl;
+    cout << "\nUsage: rivemul [OPTIONS] [MEMORY_SEGMENT]..." << endl;
+    cout << "\nArguments:" << endl;
+    cout << "   [MEMORY_SEGMENT]..." << endl;
+    cout << "           The files to load into memory. Format is file:base_addr.  If :base_addr is not provided, defaults to 0. The files and their base_addr must fit somewhere in the two memory banks" << endl;
+    cout << "\nOptions:" << endl;
+    cout << "   -r <RESET_ADDR>" << endl;
+    cout << "           The reset address" << endl;
+    cout << "\n           [default: 0x0]" << endl;
+    cout << "\n   -i" << endl;
+    cout << "           Interactive (debug) mode" << endl;
+    cout << "\n   -m <MEM_SIZE>" << endl;
+    cout << "           Memory size (in KB). This memory size will be split in 2 memory banks:" << endl;
+    cout << "\n           - one starting at address 0x00000000, half of the given size" << endl;
+    cout << "\n           - one starting at address 0x20000000, half of the give size" << endl;
+    cout << "\n           [default: 512]" << endl;
+    cout << "\n   -h, --help" << endl;
+    cout << "           Print help (see a summary with '-h')" << endl;
+    cout << "\n   -V, --version" << endl;
+    cout << "           Print version" << endl;
 }
 
 void printError(){
-    cout << "Utilisation: riscv-decode [OPTIONS] FICHIER_BIN" << endl;
-    cout << "Essaie 'riscv-decode -h' pour plus d'information." << endl;
+    cout << "rivemul [OPTIONS] [MEMORY_SEGMENT]..." << endl;
+    cout << "For more information, try 'rivemul --help'." << endl;
+}
+
+void printVersion(){
+    cout << "rivemul 0.1" << endl;
 }
